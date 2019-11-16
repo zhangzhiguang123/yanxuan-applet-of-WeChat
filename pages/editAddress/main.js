cf97be18065e5f513e8594ef2876614933112f7c
zhangzhiguang123 <1339435196@qq.com>
2019-11-16 10:48:58 +0800

    1048

--- pages/editAddress/main.js
+++ pages/editAddress/main.js
@@ -80,31 +80,42 @@ Page({
   selectAddress(e) {
     let that = this;
     let data = that.data.addrlist[e.currentTarget.dataset.index];
-    let pages = getCurrentPages();  // 当前页的数据
-    let prevPage = pages[pages.length - 2];  // 上一页的数据，也可以输出来看看有什么东西
+    let pages = getCurrentPages(); // 当前页的数据
+    let prevPage = pages[pages.length - 2]; // 上一页的数据，也可以输出来看看有什么东西
     prevPage.setData({
       address: data
     })
 
     wx.navigateBack({
-      delta:1
+      delta: 1
     })
   },
   /**
    * 重新相应编辑地址事件处理
    */
   reEditThisAddress(e) {
-   wx.navigateTo({
-     url: `reEditAdress/main?id=${e.currentTarget.dataset.index}`,
-   })
+    wx.navigateTo({
+      url: `reEditAdress/main?id=${e.currentTarget.dataset.index}`,
+    })
   },
 
-/**
+  /**
    * 添加新地址事件处理
    */
-  addAddress(){
-   wx.navigateTo({
-     url: 'reEditAdress/main'
-   })
+  addAddress() {
+    wx.navigateTo({
+      url: 'reEditAdress/main'
+    })
+  },
+  loadData() {   //用于刷新数据
+    let that = this;
+    requestData("heyushuo/address/getListAction?openId=oQmbb4sNZdxaUQZ0sfYgvtOP2S7c").then(res => { //请求后台地址列表
+      that.setData({
+        addrlist: res.data.data
+      });
+      console.log(that.data.addrlist);
+    }).catch(err => {
+      console.log(err);
+    })
   }
 })
--- pages/editAddress/reEditAdress/main.js
+++ pages/editAddress/reEditAdress/main.js
@@ -1,39 +1,66 @@
 // pages/editAddress/reEditAdress/main.js
-import { requestData } from "../../../api/rq.js";
+import {
+  requestData, sendData
+} from "../../../api/rq.js";
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-     addrInfo:{}
+    addrInfo: {},
+    name: String,
+    mobile: Number,
+    address: String,
+    address_detail: String,
+    isDefault: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-     let that = this;
-     let id = options.id||"";
-    requestData(`heyushuo/address/detailAction?id=${id}`).then(res=>{   //拿当前ID的个人信息，并赋给数据模型
-      console.log(res.data.data);
+    let that = this;
+    let id;
+    if(options.id===0){
+      id = "0";
+    }else{
+      id = options.id || "";
+    }
+    if(id!==""){
+      requestData(`heyushuo/address/detailAction?id=${id}`).then(res => { //拿当前ID的个人信息，并赋给数据模型
+        console.log(res.data.data);
+        that.setData({
+          addrInfo: res.data.data,
+          name: res.data.data.name,
+          mobile: res.data.data.mobile,
+          address: res.data.data.address,
+          address_detail: res.data.data.address_detail,
+        });
+      })
+    }else{
       that.setData({
-          addrInfo:res.data.data
+        addrInfo:{},
+        name: "",
+        mobile: Number,
+        address: String,
+        address_detail: String,
       });
-    })
+    }
+   
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-     
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-     
+
   },
 
   /**
@@ -69,5 +96,68 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  goSaveAddress() {
+    let that = this;
+   let addrId= that.data.addrInfo.id||"";
+    let sendDataObj = {
+      address: that.data.address,
+      addressId: addrId,
+      checked: that.data.isDefault,
+      detailadress: that.data.address_detail,
+      openId: "oQmbb4sNZdxaUQZ0sfYgvtOP2S7c",
+      telNumber: that.data.mobile,
+      userName: that.data.name
+    };
+    // console.log(sendDataObj);
+
+    sendData("heyushuo/address/saveAction",sendDataObj).then(res=>{
+      console.log(res.data.data);
+      if(res.data.data){
+        let pages = getCurrentPages();
+        let beforePage = pages[pages.length - 2];
+        beforePage.loadData();  //刷新上页数据
+        wx.navigateBack({
+          delta:1
+        })
+      }
+    }).catch(err=>{
+      throw err;
+    });
+
+  },
+
+  getName(e) {
+    this.setData({
+      name: e.detail.value
+    });
+  },
+
+  getMobile(e) {
+    this.setData({
+      mobile: e.detail.value
+    });
+  },
+
+  getAddress(e) {
+    this.setData({
+      address: e.detail.value
+    });
+  },
+
+  getAddress_detail(e) {
+    this.setData({
+      address_detail: e.detail.value
+    });
+  },
+
+  getIsDefault(e) {
+    let that = this;
+    this.setData({
+      isDefault: !that.data.isDefault
+    });
   }
 })
